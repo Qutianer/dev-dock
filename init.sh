6d7beec85b1d7fcca5aa83c547efbdecb02022c4
root <root@docker.test.internal>
2021-08-18 18:42:59 -0400

    Add 'make' way to build images

--- init.sh
+++ init.sh
@@ -1,26 +1,22 @@
 #/bin/bash
 
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}Pull and start local registry${NC}"
-docker run -d -p 5000:5000 --name registry -v "registry:/var/lib/registry/" registry:2
+docker run -d --rm -p 5000:5000 --name registry -v "registry:/var/lib/registry/" registry:2
 
 echo -e "${GREEN}Building nginx image and push it into local registry${NC}"
 pushd nginx
-docker build -t nginx-a .
-docker tag nginx-a localhost:5000/nginx-a
+docker build -t nginx-a -t localhost:5000/nginx-a .
 docker push localhost:5000/nginx-a
 popd
 
 echo -e "${GREEN}Building apache image and push it into local registry${NC}"
 pushd apache
-docker build -t httpd-a .
-docker tag httpd-a localhost:5000/httpd-a
+docker build -t httpd-a -t localhost:5000/httpd-a .
 docker push localhost:5000/httpd-a
 popd
-echo "done"
 
 echo -e "${GREEN}Prepare docker environment and generate index page${NC}"
 docker volume create website
